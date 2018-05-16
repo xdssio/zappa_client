f2db036f680e03aced489bf59bfe71b9f5bb416b
shareactor <yonatan@shareactor.io>
2018-05-16 13:40:21 +0200

    ZappaClient

---
+++ tests/test_zappa_client.py
@@ -0,0 +1,9 @@
+from zappa_client import ZappaClient
+
+
+def test_zappa_client():
+    function_name = '<change this>'
+    path = 'train'
+    client = ZappaClient()
+
+
---
+++ zappa_client.py
@@ -0,0 +1,88 @@
+import base64
+import json
+
+import boto3
+
+default_body = {}
+
+
+class ZappaClient:
+    def __init__(self):
+        self.client = boto3.client('lambda')
+
+    def invoke(self, function_name, path, data=default_body, http_method="GET"):
+        payload = self.get_payload(path=path, body=data, http_method=http_method)
+        return self.invoke_payload(function_name=function_name, payload=payload)
+
+    def invoke_payload(self, function_name, payload):
+        return self.parse_response(self.client.invoke(
+            FunctionName=function_name,
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            Payload=payload,
+        ))
+
+    def get_payload(self, path, body=default_body, http_method="POST", headers=None):
+        if not headers:
+            headers = self.get_headers()
+
+        payload = {
+            "body": body,
+            "headers": headers,
+            "pathParameters": {},
+            "queryStringParameters": {},
+            "requestContext": {},
+            "httpMethod": http_method,
+            "path": path
+        }
+        return json.dumps(payload)
+
+    @staticmethod
+    def get_headers():
+        headers = {
+            "Accept-Language": "en-US,en;q=0.8",
+            "Accept": "application/json",
+            "Accept-Encoding": "gzip, deflate",
+            "Content-Type": "application/json"
+        }
+        return headers
+
+    @staticmethod
+    def parse_response(response):
+        payload = response['Payload'].read()
+        json_body = json.loads(payload.decode())
+        body = json_body.get('body', None)
+        ret = None
+        if body:
+            ret = base64.b64decode(body)
+            try:
+                ret = eval(ret)
+            except Exception as e:
+                raise ValueError(e)
+
+        return ret
+
+    def get_function_environment_variables(self, function_name):
+        response = self.client.get_function_configuration(
+            FunctionName=function_name
+        )
+        environment_varibles = response['Environment']['Variables']
+        environment_varibles = {str(key): str(environment_varibles[key]) for key in environment_varibles}
+        return environment_varibles
+
+    def update_environment_variable(self, function_name, key, value=None):
+
+        environment_variables = self.get_function_environment_varibles(function_name=function_name)
+        if value:
+            environment_variables[key] = value
+        else:
+            if key in environment_variables:
+                environment_variables.pop(key)
+        environment = {'Variables': environment_variables}
+        response = self.client.update_function_configuration(FunctionName=function_name, Environment=environment)
+        try:
+            environment_varibles = response['Environment']['Variables']
+            response = {str(key): str(environment_varibles[key]) for key in environment_varibles}
+        except Exception as e:
+            response = 'Failed to get response: %s' % e
+        return response
