a37f3c94796cb9435b75329c80ab89f97e76e7c0
shareactor <yonatan@shareactor.io>
2018-05-16 14:20:29 +0200

    empty test
    can’t create tests if you don’t have a zappa function running

--- tests/test_zappa_client.py
+++ tests/test_zappa_client.py
@@ -2,8 +2,5 @@ from zappa_client import ZappaClient
 
 
 def test_zappa_client():
-    function_name = '<change this>'
-    path = 'train'
     client = ZappaClient()
-
-
+    assert client is not None
